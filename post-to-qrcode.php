902e81b50339651bebcad2a0dc94c5bdd0b2345e
mnm-54 <shahriarmun54@gmail.com>
2022-05-06 17:38:22 +0600

    display qrcode

--- post-to-qrcode.php
+++ post-to-qrcode.php
@@ -11,8 +11,21 @@ Text Domain: post-to-qrcode
 Domain Path: /languages/
 */
 
-function pqc_load_textdomain()
+function pqrc_load_textdomain()
 {
     load_plugin_textdomain('post-to-qrcode', false, dirname(__FILE__) . '/languages');
 }
-add_action('plugins_loaded', 'pqc_load_textdomain');
+add_action('plugins_loaded', 'pqrc_load_textdomain');
+
+
+function pqrc_display_qr_code($content)
+{
+    $current_post_id = get_the_ID();
+    $current_post_tittle = get_the_title($current_post_id);
+    $current_post_url = urlencode(get_the_permalink($current_post_id));
+    $image_src = sprintf("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s", $current_post_url);
+
+    $content .= sprintf("<div class='qrcode'><img src='%s' alt='%s'/></div>", $image_src, $current_post_tittle);
+    return $content;
+}
+add_filter('the_content', 'pqrc_display_qr_code');
