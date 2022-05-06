6c1f09ac1778828a88effec490a7423338ee0af1
mnm-54 <shahriarmun54@gmail.com>
2022-05-06 17:09:09 +0600

    plugin definition

---
+++ post-to-qrcode.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Post to QR code generator
+Plugin URI: 
+Description: Creates a Qr code of the post url, display under the post, which can be scaned using QR code scanner
+Version: 1.0
+Author: munem
+Author URI: https://github.com/mnm-54
+License: GPLv2 or later
+Text Domain: post-to-qrcode
+Domain Path: /languages/
+*/
