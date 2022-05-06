a5823f2f04b8de6a48a010f9a779eaf69d2002b8
mnm-54 <shahriarmun54@gmail.com>
2022-05-06 22:04:46 +0600

    apply different filter hooks

--- post-to-qrcode.php
+++ post-to-qrcode.php
@@ -20,12 +20,28 @@ add_action('plugins_loaded', 'pqrc_load_textdomain');
 
 function pqrc_display_qr_code($content)
 {
+    if (is_home()) {
+        return $content;
+    }
+
+    // apply hooks
+    // post type check
+    $excluded_type_post = apply_filters('pqrc_excluded_post_types', array());
+    // dimension hook
+    $dimension = apply_filters('pqrc_qrcode_dimension', 150);
+    // image attributes
+    $img_attributes = apply_filters('pqrc_img_attributes', '');
+
     $current_post_id = get_the_ID();
+    $current_post_type = get_post_type($current_post_id);
+    if (in_array($current_post_type, $excluded_type_post)) {
+        return $content;
+    }
     $current_post_tittle = get_the_title($current_post_id);
     $current_post_url = urlencode(get_the_permalink($current_post_id));
-    $image_src = sprintf("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s", $current_post_url);
+    $image_src = sprintf("https://api.qrserver.com/v1/create-qr-code/?size=%sx%s&data=%s", $dimension, $dimension, $current_post_url);
 
-    $content .= sprintf("<div class='qrcode'><img src='%s' alt='%s'/></div>", $image_src, $current_post_tittle);
+    $content .= sprintf("<div class='qrcode'><img %s src='%s' alt='%s'/></div>", $img_attributes, $image_src, $current_post_tittle);
     return $content;
 }
 add_filter('the_content', 'pqrc_display_qr_code');
